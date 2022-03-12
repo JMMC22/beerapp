5532ceacfb709415f40c7c1055475bf278c08de9
José María Márquez <jmmarquezcrespo@gmail.com>
2022-03-12 21:00:55 +0100

    Update bottom nav

---
+++ beerapp/lib/src/presentation/navigation/widgets/bottom_nav_bar.dart
@@ -0,0 +1,65 @@
+import 'package:beerapp/src/presentation/navigation/constants/nav_bar_items.dart';
+import 'package:beerapp/src/presentation/navigation/cubit/cubit/navigation_cubit.dart';
+import 'package:beerapp/src/themes/themes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  CustomBottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  State<CustomBottomNavigationBar> createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NavigationCubit, NavigationState>(
+        builder: (context, state) {
+      return BottomNavigationBar(
+        currentIndex: state.index,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        iconSize: 35,
+        selectedItemColor: state.index == 0
+            ? AppCustomTheme.colors.bottomNavLightBlue
+            : state.index == 1
+                ? AppCustomTheme.colors.bottomNavLightYellow
+                : AppCustomTheme.colors.bottomNavLightRed,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.house_rounded),
+          ),
+          BottomNavigationBarItem(
+            label: 'Stadistics',
+            icon: Icon(
+              Icons.bar_chart_rounded,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.person_rounded),
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              BlocProvider.of<NavigationCubit>(context)
+                  .getNavBarItem(NavBarItem.home);
+              break;
+            case 1:
+              BlocProvider.of<NavigationCubit>(context)
+                  .getNavBarItem(NavBarItem.stadistics);
+              break;
+            case 2:
+              BlocProvider.of<NavigationCubit>(context)
+                  .getNavBarItem(NavBarItem.profile);
+              break;
+          }
+        },
+      );
+    });
+  }
+}
--- beerapp/lib/src/presentation/root_page.dart
+++ beerapp/lib/src/presentation/root_page.dart
@@ -1,7 +1,7 @@
 import 'package:beerapp/src/presentation/home/view/home_page.dart';
 import 'package:beerapp/src/presentation/navigation/constants/nav_bar_items.dart';
 import 'package:beerapp/src/presentation/navigation/cubit/cubit/navigation_cubit.dart';
-import 'package:beerapp/src/presentation/navigation/widgets/bottom_nav.dart';
+import 'package:beerapp/src/presentation/navigation/widgets/bottom_nav_bar.dart';
 import 'package:beerapp/src/presentation/profile/view/profile_page.dart';
 import 'package:beerapp/src/presentation/stadistics/view/stadistics_page.dart';
 import 'package:beerapp/src/themes/themes.dart';
@@ -20,30 +20,7 @@ class _RootPageState extends State<RootPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppCustomTheme.colors.backgroundGrey,
-        bottomNavigationBar: BlocBuilder<NavigationCubit, NavigationState>(
-          builder: (context, state) {
-            return CustomBottomNav(
-              currentIndex: state.index,
-              onTap: (index) {
-                print(index);
-                switch (index) {
-                  case 0:
-                    BlocProvider.of<NavigationCubit>(context)
-                        .getNavBarItem(NavBarItem.home);
-                    break;
-                  case 1:
-                    BlocProvider.of<NavigationCubit>(context)
-                        .getNavBarItem(NavBarItem.stadistics);
-                    break;
-                  case 2:
-                    BlocProvider.of<NavigationCubit>(context)
-                        .getNavBarItem(NavBarItem.profile);
-                    break;
-                }
-              },
-            );
-          },
-        ),
+        bottomNavigationBar: CustomBottomNavigationBar(),
         body: BlocBuilder<NavigationCubit, NavigationState>(
           builder: (context, state) {
             switch (state.navBarItem) {

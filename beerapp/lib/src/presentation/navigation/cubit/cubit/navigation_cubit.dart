1248087298f35f47f8ffa07ed0e733947179f976
José María Márquez <jmmarquezcrespo@gmail.com>
2022-03-12 12:03:50 +0100

    Add navigation cubit

---
+++ beerapp/lib/src/presentation/navigation/cubit/cubit/navigation_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:beerapp/src/presentation/navigation/constants/nav_bar_items.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'navigation_state.dart';
+
+class NavigationCubit extends Cubit<NavigationState> {
+  NavigationCubit()
+      : super(const NavigationState(NavBarItem.home, 0)); // Initial state
+
+  void getNavBarItem(NavBarItem navBarItem) {
+    switch (navBarItem) {
+      case NavBarItem.home:
+        emit(const NavigationState(NavBarItem.home, 0));
+        break;
+      case NavBarItem.stadistics:
+        emit(const NavigationState(NavBarItem.stadistics, 0));
+        break;
+      case NavBarItem.profile:
+        emit(const NavigationState(NavBarItem.profile, 0));
+        break;
+    }
+  }
+}

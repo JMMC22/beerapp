305c7c119621633993493d7554bdc35f17b42e9e
José María Márquez <jmmarquezcrespo@gmail.com>
2022-03-15 10:36:21 +0100

    Add firestore to pods and fix problem build time

--- beerapp/lib/main.dart
+++ beerapp/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:beerapp/src/presentation/navigation/cubit/cubit/navigation_cubit.dart';
 import 'package:beerapp/src/presentation/root_page.dart';
 import 'package:beerapp/src/routes/router_generator.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
